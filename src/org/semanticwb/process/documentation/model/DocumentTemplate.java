3a64ea42cc1d948a6b22d1802b2a1aa407687545
Hasdai Pacheco <me@hasdaipacheco.com>
2016-03-01 19:00:58 +0000

    Agregado método para obtener los números de versión de las plantillas Sobrescrito método addDocumentSection para establecer la relación padre/hijo de los elementos Agregado método para clonar la plantilla

--- src/org/semanticwb/process/documentation/model/DocumentTemplate.java
+++ src/org/semanticwb/process/documentation/model/DocumentTemplate.java
@@ -1,10 +1,16 @@
 package org.semanticwb.process.documentation.model;
 
+import java.text.NumberFormat;
+import java.util.Iterator;
+import org.semanticwb.model.SWBComparator;
+import org.semanticwb.model.WebSite;
+
 /**
  * Clase que encapsula la información de una plantilla para documentación.
  */
 public class DocumentTemplate extends org.semanticwb.process.documentation.model.base.DocumentTemplateBase 
 {
+    private static final NumberFormat numf = NumberFormat.getNumberInstance();
     /**
      * Constructor. Crea una nueva instancia de una plantilla de documentación.
      * @param base 
@@ -12,5 +18,64 @@ public class DocumentTemplate extends org.semanticwb.process.documentation.model
     public DocumentTemplate(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+    
+    /**
+     * Obtiene el valor de la siguiente versión, de acuerdo al valor proporcionado.
+     * @param prevValue Valor de versión tomado como punto de partida.
+     * @return Número de la siguiente versión.
+     */
+    public static String getNextVersionValue(String prevValue) {
+        String prev = prevValue;
+        if (null == prevValue || prevValue.isEmpty()) {
+            return "1.0";
+        }
+        
+        numf.setMaximumFractionDigits(1);
+
+        String sver = prev;
+        double f = Double.parseDouble(sver);
+        f = f + 0.10D;
+
+        String sfver = numf.format(f);
+        if (!sfver.contains(".")) {
+            sfver = "" + (float) f;
+        }
+        sver = sfver;
+        return sver;
+    }
+
+    @Override
+    public void addDocumentSection(DocumentSection value) {
+        super.addDocumentSection(value);
+        if (null != value) value.setParentTemplate(this);
+    }
+    
+    /**
+     * Crea una copia de la plantilla.
+     * @return 
+     */
+    public DocumentTemplate cloneTemplate() {
+        DocumentTemplate ret = null;
+        WebSite model = (WebSite)getSemanticObject().getModel().getModelObject().getGenericInstance();
+        
+        if (null != model) {
+            ret = DocumentTemplate.ClassMgr.createDocumentTemplate(model);
+             
+            //Set common properties
+            ret.setTitle(getTitle());
+            ret.setVersionValue(getNextVersionValue(null));
+            ret.setDescription(getDescription());
+
+            //Clone section elements
+            Iterator<DocumentSection> sections = SWBComparator.sortSortableObject(listDocumentSections());
+            while (sections.hasNext()) {
+                DocumentSection section = sections.next();
+                DocumentSection cloned = section.cloneDocumentSection();
+                //cloned.setParentTemplate(ret);
+                ret.addDocumentSection(cloned);
+            }
+        }
+        return ret;
     }
 }
