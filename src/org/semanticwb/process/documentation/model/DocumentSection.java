29e571ba97c72a9fd30eebf5885c5e82248f875e
Hasdai Pacheco <me@hasdaipacheco.com>
2016-03-01 18:58:39 +0000

    Agregado método para clonar una sección de documentación

--- src/org/semanticwb/process/documentation/model/DocumentSection.java
+++ src/org/semanticwb/process/documentation/model/DocumentSection.java
@@ -1,5 +1,7 @@
 package org.semanticwb.process.documentation.model;
 
+import org.semanticwb.model.WebSite;
+
 /**
  * Clase que encapsula la información de una sección de documentación.
  */
@@ -12,5 +14,26 @@ public class DocumentSection extends org.semanticwb.process.documentation.model.
     public DocumentSection(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+    
+    /**
+     * Crea una copia del DocumentSection
+     * @return DocumentSection
+     */
+    public DocumentSection cloneDocumentSection() {
+        DocumentSection ret = null;
+        WebSite model = (WebSite)getSemanticObject().getModel().getModelObject().getGenericInstance();
+        
+        if (null != model) {
+            ret = DocumentSection.ClassMgr.createDocumentSection(model);
+            ret.setConfigData(getConfigData());
+            ret.setIndex(getIndex());
+            ret.setSectionType(getSectionType());
+            ret.setTitle(getTitle());
+            ret.setVisibleProperties(getVisibleProperties());
+            ret.setActive(isActive());
+        }
+        
+        return ret;
     }
 }
